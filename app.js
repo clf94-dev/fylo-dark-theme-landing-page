7af3e9359252fc0a57a184099179b4d1c0755ca5
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-01 10:59:54 +0200

    email validation added

--- app.js
+++ app.js
@@ -18,3 +18,45 @@ const navSlide = () => {
 };
 
 navSlide();
+
+
+const notifyBTN = document.querySelector('button.btn-cta');
+const error = document.querySelector('.error');
+const emailInput = document.querySelector('input.email');
+const myForm = document.querySelector('form');
+
+notifyBTN.addEventListener('click', validation);
+let validError = false;
+
+function validation(e) {
+
+    e.preventDefault();
+    error.classList.add('hide');
+    emailInput.style.borderColor = '#ddd';
+    if (emailInput.value.length == 0) {
+        addError(emailInput, 'Required Field');
+    } else {
+        let exp = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        let result = exp.test(emailInput.value);
+        if (!result) {
+            addError(emailInput, 'Please provide a valid email address');
+            validError = true;
+        } else {
+            validError = false;
+            console.log(validError);
+        }
+    }
+    if (!validError) {
+        console.log("Submit");
+        myForm.submit();
+    }
+
+}
+
+function addError(input, mes) {
+    let temp = input.nextElementSibling;
+    temp.classList.remove('hide');
+    temp.textContent = mes;
+    input.style.borderColor = 'red';
+    input.focus();
+}
